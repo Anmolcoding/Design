6d57839aa9d492adb92c4a8ee22cf9a2ff6af4f8
Anmol Pandey <105305940+Anmolcoding@users.noreply.github.com>
2024-08-09 17:44:39 +0530

    Create main.tsx

---
+++ main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+// import Header from "./Components/Header";
+// import ConfirmPay from "./Components/ConfirmPay";
+import Details from "./Components/Details";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    {/* <Header />
+    <ConfirmPay /> */}
+    <Details />
+  </React.StrictMode>
+);
